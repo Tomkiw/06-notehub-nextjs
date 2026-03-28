82cf3c674d0dda71a77a62cccbd52224fcd3c090
Tomkiv_Ihor <igortomkiw3@gmail.com>
2026-03-28 11:19:44 +0200

    feat: Add note details and error handling components, update notes page to use new structure

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -2,13 +2,11 @@
 // винесіть в окремий файл компонента app/notes/Notes.client.tsx.
 
 "use client";
-
 import { useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
-import { fetchNoteById, fetchNotes } from "@/lib/api";
+import { fetchNotes } from "@/lib/api";
 import css from "./NotePage.module.css";
-import { useParams } from "next/navigation";
 
 import Pagination from "@/components/Pagination/Pagination";
 import NoteList from "@/components/NoteList/NoteList";
@@ -16,22 +14,21 @@ import Modal from "@/components/Modal/Modal";
 import SearchBox from "@/components/SearchBox/SearchBox";
 import NoteForm from "@/components/NoteForm/NoteForm";
 
-export default function NoteDetailsClient() {
+export default function NotesClient() {
   const [page, setPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const { id } = useParams<{ id: string }>(); // Читає Id з URL
-
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["notes", id],
-    queryFn: () => fetchNoteById(id),
-  });
 
   const handleSearch = useDebouncedCallback((value: string) => {
     setSearchQuery(value);
     setPage(1);
   }, 500);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["notes", searchQuery, page],
+    queryFn: () => fetchNotes({ page, perPage: 12, search: searchQuery }),
+    placeholderData: keepPreviousData,
+  });
 
   return (
     <div className={css.app}>
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,33 +1,34 @@
 // notes – сторінка списку нотатків. На цій сторінці відображається перелік усіх створених нотаток.
 // Реалізовано функцію пошуку за ключовим словом, а також можливість створення нової нотатки.
 
-import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
-import { fetchNoteById } from "@/lib/api";
+import NotesClient from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
 import {
   QueryClient,
   HydrationBoundary,
   dehydrate,
 } from "@tanstack/react-query";
 
-interface NoteProps {
-  params: Promise<{ id: string }>;
+interface NotesPageProps {
+  searchParams: Promise<{ page?: string; search?: string }>;
 }
 
-async function NoteDetailsPage({ params }: NoteProps) {
-  const { id } = await params;
+async function Notes({ searchParams }: NotesPageProps) {
+  const { page, search } = await searchParams;
+  const currentPage = Number(page ?? "1");
 
   const queryClient = new QueryClient();
 
   queryClient.prefetchQuery({
-    queryKey: ["notes", id],
-    queryFn: () => fetchNoteById(id),
+    queryKey: ["notes", page, search],
+    queryFn: () => fetchNotes({ page: currentPage, perPage: 12, search }),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsClient />
+      <NotesClient />
     </HydrationBoundary>
   );
 }
 
-export default NoteDetailsPage;
+export default Notes;
